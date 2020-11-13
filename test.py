b51b5d17450bf5a19dd49aaa0879b9f2363b10cd
Sid <sidkhamithkar@gmail.com>
2020-11-13 20:07:15 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get('https://classroom.google.com/u/0/c/MTYxODI3NTY3Mjk0')
+elem = driver.find_element_by_name('identifier')
+time.sleep(1)
+elem.send_keys('s1050siddharth16103@elearnkvsroblr.in')
+elem.send_keys(Keys.ENTER)
+time.sleep(1)
+psw = driver.find_element_by_name('password')
+psw.send_keys('Kingpin@123')
+psw.send_keys(Keys.RETURN)
+time.sleep(10)
+driver.find_element_by_class_name("zTrXGf").click()
+time.sleep(1)
+post = driver.find_element_by_class_name('tL9Q4c')
+post.send_keys('This post was made by an automation script')
+driver.find_element_by_class_name('Fxmcu').click()
